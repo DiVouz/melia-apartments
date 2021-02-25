7fcc8b74744d303420b426fb5295a006fd7ffacd
DiVouz <divouz@outlook.com>
2021-02-25 03:30:03 +0200

    some more work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
     setStickClass();
+
+    squareTheRoundImgs();
+});
+
+$(window).resize(function() {
+    squareTheRoundImgs();
 });
 
 function setStickClass() {
@@ -11,4 +17,35 @@ function setStickClass() {
     );
 
     observer.observe(stickyElm);
+}
+
+function toggleExtraFeatures(forceOpen) {
+    let element = $("#extraFeatures");
+    
+    if (element) {
+        if (element.is(':hidden')) {
+            $("#extraFeaturesButton").html('Show Less');
+        } else {
+            $("#extraFeaturesButton").html('Show More');
+        }
+
+        element.stop(true, false).slideToggle("slow");
+    }
+}
+
+function scrollToTab(tabId) {
+    let element = $(`#${tabId}`);
+    let navbarElement = $("#navbar");
+
+    if (element && navbarElement) {
+        $('html, body').stop(true, false).animate({
+            scrollTop: element.offset().top - navbarElement.height() + 2
+        }, 1000);
+    }
+}
+
+function squareTheRoundImgs() {
+    $(".square_round_img").each(function(index) {
+        $(this).height($(this).width());
+    });
 }
